3c4541ef924fb75ee791c1df79ad0ae51267905c
William <catcat811@gmail.com>
2012-05-06 15:57:18 +0800

    ^ template

---
+++ templates/sun/js/start.js
@@ -0,0 +1,15 @@
+var Sun = window.Sun || {};
+
+Sun.Table = (funciton($) {
+    
+    var thisObject = {
+        init: function() {
+        }
+    }
+    
+    return thisObject;
+})(document.id);
+
+window.addEvent("domready", function(){
+	Sun.Table.init();
+});
